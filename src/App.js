38fe8b7b552435a629375cd114aa0e3dfa0ac788
MD AWAIZ <awaizkhanmd@gmail.com>
2023-01-17 02:01:01 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import { NavBar } from './components/Navbar';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Project';
+import { Contact } from "./components/Contact";
+
 
 function App() {
   return (
     <div className="App">
   <NavBar/>
   <Banner/>
-
-
+<Skills/>
+<Projects/>
+<Contact></Contact>
     </div>
   );
 }
